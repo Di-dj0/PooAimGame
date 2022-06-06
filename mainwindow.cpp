e471bd4f580c91d88fbde3d4e18ddf4d6bd2b34c
Di-dj0 <luiz.g.comarella@gmail.com>
2022-06-06 12:57:09 -0300

    Second Commit 12-05

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,20 +3,35 @@
 mainWindow::mainWindow(QWidget *parent)
     : QMainWindow{parent}
 {
-    this->setGeometry(100,100,800,600);
+    this->setGeometry(350,100,300,300);
 
     minigame1 = new QPushButton("Minigame 1", this);
-    minigame1->setGeometry(690,20,90,40);
+    minigame1->setGeometry(50,150,200,50);
 
     minigame2 = new QPushButton("Minigame 2", this);
-    minigame2->setGeometry(690,80,90,40);
+    minigame2->setGeometry(50,200,200,50);
+
+    game1 = new QWindow();
+    game1->setGeometry(350,100,600,600);
+
 
     connect(minigame1, SIGNAL(clicked(bool)), this, SLOT(pequenoJogo1()));
 
     connect(minigame2, SIGNAL(clicked(bool)), this, SLOT(pequenoJogo2()));
+
 }
 
 void mainWindow::pequenoJogo1(){
+    double time = QInputDialog::getDouble(this,"Espera input", "Digite o tempo para o jogo começar");
+    int dificult = QInputDialog::getInt(this,"Escolha a dificuldade", "Digite um valor entre 1 e 3 para a dificuldade",
+                                        0, 1, 3);
+
+
+    this->hide();
+
+    game1->show();
+
+
 
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -4,12 +4,15 @@
 #include <QMainWindow>
 #include <QPushButton>
 #include <QInputDialog>
+#include <QWindow>
+#include <QTimer>
 
 class mainWindow : public QMainWindow
 {
     Q_OBJECT
     QPushButton *minigame1, *minigame2;
     QInputDialog *userInput;
+    QWindow *game1;
 public:
     explicit mainWindow(QWidget *parent = nullptr);
 
